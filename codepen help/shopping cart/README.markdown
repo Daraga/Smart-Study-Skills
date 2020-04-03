e852b6d5424b45d9e32ad5dbccd742b5b4f4317e
Daraga <38896466+Daraga@users.noreply.github.com>
2020-04-03 15:53:04 -0400

    upload

---
+++ codepen help/shopping cart/README.markdown
@@ -0,0 +1,4 @@
+# Flat Cart [codepen comp]
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/willpaige/pen/gwxbD](https://codepen.io/willpaige/pen/gwxbD).
+
+ Run out of time to work on it!!! 
---
+++ js/script.js
@@ -0,0 +1,6 @@
+function multiplyBy() {
+    num1 = document.getEmlementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+
+}
